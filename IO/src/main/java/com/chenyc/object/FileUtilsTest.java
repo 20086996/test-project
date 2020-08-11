5a7ff11e73f1d7b622f68feebf8fc03e4f88c0fa
chenyc <345876035@qq.com>
2020-08-11 22:46:21 +0800

    socket

--- IO/src/main/java/com/chenyc/object/FileUtilsTest.java
+++ IO/src/main/java/com/chenyc/object/FileUtilsTest.java
@@ -1,7 +1,6 @@
 package com.chenyc.object;
 
-//import org.apache.commons.io.FileUtils;
-
+import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,13 +11,13 @@ import java.io.IOException;
 public class FileUtilsTest {
 
     public static void main(String[] args) {
-//        File srcFile = new File("day10\\爱情与友情.jpg");
-//        File destFile = new File("day10\\爱情与友情2.jpg");
-//
-//        try {
-//            FileUtils.copyFile(srcFile,destFile);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        File srcFile = new File("day10\\爱情与友情.jpg");
+        File destFile = new File("day10\\爱情与友情2.jpg");
+
+        try {
+            FileUtils.copyFile(srcFile,destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
