772f6aec1306f250dda1d32a11c2ffb7f23bcf2c
chenyc <345876035@qq.com>
2020-08-16 09:48:11 +0800

    proxy

--- java8/src/main/java/com/chenyc/java3/StreamAPITest2.java
+++ java8/src/main/java/com/chenyc/java3/StreamAPITest2.java
@@ -4,10 +4,7 @@ import com.chenyc.java2.Employee;
 import com.chenyc.java2.EmployeeData;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -110,8 +107,65 @@ public class StreamAPITest2 {
         Set<Employee> employeeSet = employees.stream().filter(e -> e.getSalary() > 6000).collect(Collectors.toSet());
         employeeSet.forEach(System.out::println);
 
+        System.out.println("--------------------------------------");
+        List<String> collect = employees.stream().map(Employee::getName).collect(Collectors.toList());
+        collect.forEach(System.out::print);
 
+        System.out.println("--------------------------------------");
+        Set<String> collect1 = employees.stream().map(Employee::getName).collect(Collectors.toSet());
+        collect1.forEach(System.out::println);
 
+        System.out.println("--------------------------------------");
+        HashSet<String> collect2 = employees.stream().map(Employee::getName).collect(Collectors.toCollection(HashSet::new));
+        collect2.forEach(System.out::println);
 
+        System.out.println("--------------------------------------");
+        Long collect3 = employees.stream().map(Employee::getName).collect(Collectors.counting());
+        System.out.println(collect3);
+
+        System.out.println("--------------------------------------");
+        Double collect4 = employees.stream().collect(Collectors.averagingDouble(e -> e.getSalary()));
+        System.out.println(collect4);
+
+        System.out.println("--------------------------------------");
+        Double collect5 = employees.stream().collect(Collectors.summingDouble(Employee::getSalary));
+        System.out.println(collect5);
+
+        System.out.println("--------------------------------------");
+        Optional<Double> collect6 = employees.stream().map(Employee::getSalary).collect(Collectors.maxBy(Double::compareTo));
+        System.out.println(collect5.getClass());
+
+        System.out.println("--------------------------------------");
+        Optional<Double> collect7 = employees.stream().map(Employee::getSalary).collect(Collectors.minBy(Double::compareTo));
+        System.out.println(collect7.getClass());
+
+        System.out.println("--------------------------------------");
+        Map<Integer, List<Employee>> collect8 = employees.stream().collect(Collectors.groupingBy(Employee::getAge));
+        System.out.println(collect8);
+
+        System.out.println("--------------------------------------");
+        Map<String, Map<String, List<Employee>>> collect9 = employees.stream().collect(Collectors.groupingBy(Employee::getName, Collectors.groupingBy(e -> {
+            if (e.getAge() <= 35) {
+                return "青年";
+            } else {
+                return "老年";
+            }
+        })));
+        System.out.println(collect9);
+
+        System.out.println("--------------------------------------");
+        Map<Boolean, List<Employee>> collect10 = employees.stream().collect(Collectors.partitioningBy(e -> e.getSalary() > 8000));
+        System.out.println(collect10);
+
+        System.out.println("--------------------------------------");
+        DoubleSummaryStatistics collect11 = employees.stream().collect(Collectors.summarizingDouble(Employee::getSalary));
+        System.out.println(collect11.getAverage());
+        System.out.println(collect11.getMax());
+        System.out.println(collect11.getMin());
+        System.out.println(collect11.getSum());
+
+        System.out.println("--------------------------------------");
+        String collect12 = employees.stream().map(Employee::getName).collect(Collectors.joining(","));
+        System.out.println(collect12);
     }
 }
