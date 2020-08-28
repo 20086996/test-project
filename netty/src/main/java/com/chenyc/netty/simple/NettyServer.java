35244065a4843966da9314944d1b058a894ba364
chenyuancheng <chenyuancheng@huobi.com>
2020-08-28 23:14:53 +0800

    netty

--- netty/src/main/java/com/chenyc/netty/simple/NettyServer.java
+++ netty/src/main/java/com/chenyc/netty/simple/NettyServer.java
@@ -7,6 +7,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.junit.Test;
 
 public class NettyServer {
     public static void main(String[] args) throws Exception {
@@ -15,6 +16,18 @@ public class NettyServer {
         //2、bossGroup 只是处理连接请求，真正的和客户端业务处理，会交给 workerGroup 完成
         //3、两个都是无限循环
         //4、bossGroup 和 workerGroup 含有的子线程（NioEventLoop）个数为实际 cpu 核数 * 2
+        //private static final int DEFAULT_EVENT_LOOP_THREADS = Math.max(1, SystemPropertyUtil.getInt("io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
+        //16个nioEventLoop
+        //
+        //1、netty抽象出两组线程池，BossGroup专门负责接收客户端连接，WorkerGroup专门负责接收网络读写操作
+        //2、bossGroup 和 workerGroup 含有的子线程（NioEventLoop）个数为实际 cpu 核数 * 2
+        //3、NioEventLoop表示一个不断循环执行处理任务的线程，每个NioEventLoop都有一个selector，用来监听绑定在其上的SocketChannel网络通道
+        //4、NioEventLoop内部采用串行化设计，从消息的读取-》解码-》处理-》编码-》编码-》发送，始终由IO线程NioEventLoop负责
+            //每个NioEventLoop下含有多个NioEventLoop
+            //每个NioEventLoop的Selector上含有一个Selector,一个taskQueue
+            //每个NioEventLoop的Selector上可以注册监听多个NioChannel
+            //每个NioChannel只会绑定在唯一的NioEventLoop上
+            //m每个NioChannel都有绑定有一个自己的channelPipeline
         NioEventLoopGroup bossGroup = new NioEventLoopGroup();
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
@@ -35,6 +48,8 @@ public class NettyServer {
                         //给 pipeline 添加处理器，每当有连接accept时，就会运行到此处。
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
+                            System.out.println("客戶SocketChannel hashcode="+ch.hashCode());
+                            //可以使用一个集合管理SocketChannel，再推送消息时可以将业务加入到各个channel对应的NIOEventLoop的taskQueue或者scheduleTaskQueue中
                             ch.pipeline().addLast(new NettyServerHandler());
                         }
                     });
@@ -51,5 +66,10 @@ public class NettyServer {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
+    }
+
+    @Test
+    public void test02(){
+
     }
 }
--- netty/src/main/java/com/chenyc/netty/simple/NettyServerHandler.java
+++ netty/src/main/java/com/chenyc/netty/simple/NettyServerHandler.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
+import java.util.concurrent.TimeUnit;
+
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     /**
      *读取客户端发送过来的消息
@@ -15,10 +17,49 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("服务器读取线程：" + Thread.currentThread().getName());
-        System.out.println("server ctx = " + ctx);
-        ByteBuf buf = (ByteBuf) msg;
 
+        //提交到taskQueue中异步执行
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("当前处理线程："+Thread.currentThread().getName());
+                    Thread.sleep(1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("处理费时业务一了，才完成",CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("当前处理线程："+Thread.currentThread().getName());
+                    Thread.sleep(1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("处理费时业务二了，才完成",CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //用户自定义定时任务-》该任务提交到scheduleTaskQueue
+        ctx.channel().eventLoop().schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("当前处理线程："+Thread.currentThread().getName());
+                ctx.writeAndFlush(Unpooled.copiedBuffer("处理费时业务三了，才完成",CharsetUtil.UTF_8));
+            }
+        }, 5, TimeUnit.SECONDS);
+
+        System.out.println("当前处理线程：" + Thread.currentThread().getName());
+//        System.out.println("server ctx = " + ctx);
+
+
+        ByteBuf buf = (ByteBuf) msg;
         System.out.println("客户端发送的消息是：" + buf.toString(CharsetUtil.UTF_8));
         System.out.println("客户端地址：" + ctx.channel().remoteAddress());
     }
