8d22b59047785688c303a9f1f12d0f25c2b27dea
chenyuancheng <chenyuancheng@huobi.com>
2021-03-30 19:27:43 +0800

    socket

---
+++ spring-cloud2020/cloud-api-commons/src/main/java/com/chenyc/springcloud/entities/Payment.java
@@ -0,0 +1,19 @@
+package com.chenyc.springcloud.entities;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author chenyc
+ * @create 2021-03-29 16:07
+ */
+@Data
+public class Payment implements Serializable {
+    private  long id;
+    private String serial;
+
+    public Payment(){
+
+    }
+}
