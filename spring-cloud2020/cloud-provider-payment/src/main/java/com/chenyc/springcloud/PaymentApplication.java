f92136532e31d48d0b91e9bcf4d5fe653d44a94d
chenyuancheng <chenyuancheng@huobi.com>
2021-04-02 10:49:43 +0800

    springcloud

--- spring-cloud2020/cloud-provider-payment/src/main/java/com/chenyc/springcloud/PaymentApplication.java
+++ spring-cloud2020/cloud-provider-payment/src/main/java/com/chenyc/springcloud/PaymentApplication.java
@@ -2,6 +2,7 @@ package com.chenyc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient//添加该注解
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
--- spring-cloud2020/cloud-provider-payment/src/main/java/com/chenyc/springcloud/controller/PaymentController.java
+++ spring-cloud2020/cloud-provider-payment/src/main/java/com/chenyc/springcloud/controller/PaymentController.java
@@ -4,10 +4,14 @@ import com.chenyc.springcloud.entities.CommonResult;
 import com.chenyc.springcloud.entities.Payment;
 import com.chenyc.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -16,6 +20,8 @@ public class PaymentController{
     private PaymentService paymentService;
     @Value("${server.port}")
     private String serverPort;//添加serverPort
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody  Payment payment)
@@ -43,4 +49,21 @@ public class PaymentController{
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
         }
     }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery()
+    {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*****element: "+element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
+    }
+
 }
